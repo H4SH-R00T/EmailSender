7cf74cbb3b32dcddef7c3f39a285de2f7c07f95e
H4SH-R00T <marco.caspo@gmail.com>
2017-01-25 15:54:40 -0800

    FirstTwoFunctions
    I've added two function to send an email giving user, pass, from, to,
    subject and message

--- EmailSender/Mail.vb
+++ EmailSender/Mail.vb
@@ -1,3 +1,60 @@
-﻿Module Mail
+﻿Imports System.Net.Mail
 
+Module Mail
+
+    'inputs: user, pass, from, to, subject, message, server, port
+
+    Public Function sendEmail(ByVal user As String, ByVal pass As String, ByVal from As String, ByVal recipient As String, ByVal subject As String, ByVal message As String, ByVal server As String, ByVal port As Integer) As Boolean
+        Dim UserName As String = user
+        Dim mail As MailMessage = New MailMessage
+        Dim t As Boolean
+        t = True
+
+        mail.From = New MailAddress(UserName)
+        mail.To.Add(New MailAddress(recipient))
+        mail.Subject = subject
+        mail.Body = message
+
+        mail.IsBodyHtml = True
+
+        Dim client As SmtpClient = New SmtpClient(server, port)
+        client.EnableSsl = True
+        client.UseDefaultCredentials = False
+        client.Credentials = New System.Net.NetworkCredential(UserName, pass)
+        Try
+            client.Send(mail)
+        Catch ex As Exception
+            t = False
+        End Try
+
+        Return t
+
+    End Function
+    'inputs: user, pass, from, to, subject, message, "smptp.gmail.com", 587
+    Public Function sendEmail(ByVal user As String, ByVal pass As String, ByVal from As String, ByVal recipient As String, ByVal subject As String, ByVal message As String) As Boolean
+        Dim UserName As String = user
+        Dim mail As MailMessage = New MailMessage
+        Dim t As Boolean
+        t = True
+
+        mail.From = New MailAddress(UserName)
+        mail.To.Add(New MailAddress(recipient))
+        mail.Subject = subject
+        mail.Body = message
+
+        mail.IsBodyHtml = True
+
+        Dim client As SmtpClient = New SmtpClient("smtp.gmail.com", 587)
+        client.EnableSsl = True
+        client.UseDefaultCredentials = False
+        client.Credentials = New System.Net.NetworkCredential(UserName, pass)
+        Try
+            client.Send(mail)
+        Catch ex As Exception
+            t = False
+        End Try
+
+        Return t
+
+    End Function
 End Module

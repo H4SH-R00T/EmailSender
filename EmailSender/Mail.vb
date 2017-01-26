760af3628396a1eefb32bc3cf4d451151c8885c2
H4SH-R00T <marco.caspo@gmail.com>
2017-01-25 22:08:02 -0800

    CorrectionModuleMail

--- EmailSender/Mail.vb
+++ EmailSender/Mail.vb
@@ -4,30 +4,34 @@ Module Mail
 
     'inputs: user, pass, from, to, subject, message, server, port, ssl
 
-    Public Function sendEmail(ByVal user As String, ByVal pass As String, ByVal from As String, ByVal recipient As String, ByVal subject As String, ByVal message As String, ByVal server As String, ByVal port As Integer, ByVal ssl As Boolean) As Boolean
+    Public Function sendEmail(ByVal user As String, ByVal pass As String, ByVal from As String, ByVal recipient As String, ByVal subject As String, ByVal message As String, ByVal server As String, ByVal port As Integer, ByVal ssl As Boolean) As String
         Dim UserName As String = user
         Dim mail As MailMessage = New MailMessage
         Dim t As Boolean
         t = True
 
-        mail.From = New MailAddress(from)
-        mail.To.Add(New MailAddress(recipient))
-        mail.Subject = subject
-        mail.Body = message
-
-        mail.IsBodyHtml = True
-
-        Dim client As SmtpClient = New SmtpClient(server, port)
-        client.EnableSsl = ssl
-        client.UseDefaultCredentials = False
-        client.Credentials = New System.Net.NetworkCredential(UserName, pass)
         Try
+            mail.From = New MailAddress(from)
+            mail.To.Add(New MailAddress(recipient))
+            mail.Subject = subject
+            mail.Body = message
+
+            mail.IsBodyHtml = True
+
+            Dim client As SmtpClient = New SmtpClient(server, port)
+            client.EnableSsl = ssl
+            client.UseDefaultCredentials = False
+            client.Credentials = New System.Net.NetworkCredential(UserName, pass)
+
             client.Send(mail)
         Catch ex As Exception
+            Return ex.ToString
             t = False
         End Try
 
-        Return t
+        If t Then Return "True"
+
+
 
     End Function
     'inputs: user, pass, from, to, subject, message, Google server
@@ -37,7 +41,7 @@ Module Mail
         Dim t As Boolean
         t = True
 
-        mail.From = New MailAddress(UserName)
+        mail.From = New MailAddress(from)
         mail.To.Add(New MailAddress(recipient))
         mail.Subject = subject
         mail.Body = message
